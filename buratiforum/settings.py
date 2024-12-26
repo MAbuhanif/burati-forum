6efb23b5d6e99601440fee83ee419a7e88c0bf9d
Mundino Hamure <mundinoabete@gmail.com>
2024-12-26 12:10:12 +0000

    Adds a Question model

--- buratiforum/settings.py
+++ buratiforum/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n=p0tk74s*l50y6up_=7b42o8k)+)t5j2+we(56rim^r9jw)yi'
+# SECRET_KEY = 'django-insecure-n=p0tk74s*l50y6up_=7b42o8k)+)t5j2+we(56rim^r9jw)yi'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -87,6 +88,11 @@ WSGI_APPLICATION = 'buratiforum.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
