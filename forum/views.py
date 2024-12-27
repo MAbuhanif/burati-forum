0c01ffa83a9c05b7181e12102b6adcc09a1e9c37
Mundino Hamure <mundinoabete@gmail.com>
2024-12-27 20:08:57 +0000

    Add static files

--- forum/views.py
+++ forum/views.py
@@ -6,4 +6,5 @@ from .models import Question
 # Create your views here.
 class QuestionList(generic.ListView):
     queryset = Question.objects.all()
-    template_name = "question_list.html"
+    template_name = "forum/index.html"
+    paginate_by = 6
