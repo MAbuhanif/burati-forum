8341b1fd4cb4163fb00abcce168899dee3fa167c
Mundino Hamure <mundinoabete@gmail.com>
2024-12-27 19:11:02 +0000

    Add and load question list file

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Question, Answer
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Question)
+class QuestionAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug','author', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
 
 # Register your models here.
-admin.site.register(Question)
 admin.site.register(Answer)
