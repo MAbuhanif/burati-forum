5598ce04c70928d7560d450333dd7e1db7f9b863
Mundino Hamure <mundinoabete@gmail.com>
2024-12-28 11:50:41 +0000

    Add question_detail view

--- forum/urls.py
+++ forum/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.QuestionList.as_view(), name='home'),
+    path('<slug:slug>/', views.question_detail, name='question_detail'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Question
 
@@ -8,3 +8,28 @@ class QuestionList(generic.ListView):
     queryset = Question.objects.all()
     template_name = "forum/index.html"
     paginate_by = 6
+
+
+
+def question_detail(request, slug):
+    """
+    Display an individual :model:`question.Post`.
+
+    **Context**
+
+    ``question``
+        An instance of :model:`forum.Question`.
+
+    **Template:**
+
+    :template:`forum/question_detail.html`
+    """
+
+    queryset = Question.objects.all()
+    question = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "forum/question_detail.html",
+        {"question": question},
+    )
