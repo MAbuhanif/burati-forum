d1828bba039cb886c707c03fd1e8ceb3f65a3477
MAbuhanif <mundinoabete@gmail.com>
2025-02-03 17:58:06 +0000

    Enhance user profile page with additional details and update profile functionality

--- forumuser/views.py
+++ forumuser/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Profile
-
-
+from .forms import UserForm, ProfileForm
 
 @login_required
 def profile(request):
-    profile = Profile.objects.all().order_by('user').first()
-    return render(request, 'forumuser/profile.html', {'profile': profile})
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=profile)
+
+    return render(request, 'forumuser/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'profile': profile
+    })
